805ee4bf00a17376e7a092fbec9ec6f9dfc835d7
dmarkbreiter <dmarkbreiter@nhm.org>
2021-12-18 14:45:05 -0800

    Added json entry point to app

---
+++ app.js
@@ -0,0 +1,263 @@
+
+const data = {
+  desks: [
+    {
+      name: 'Ko desk',
+      image: 'https://cdn.shopify.com/s/files/1/0834/1859/products/KO_DESK_2_1024x1024.jpg?v=1603770115',
+      link: 'https://akronst.com/products/ko-desk',
+      company: 'Akron Street',
+      attributes: {
+        price: '$1000',
+        dimensions: '52 x 24 x 30"',
+      }
+    },
+    {
+      name: 'Modern Neo Desk',
+      image: 'https://images.squarespace-cdn.com/content/v1/5ad9784750a54fb24f55739e/1524350204027-59AP1DQ3SYOAXAW97OHX/OakTable-02.jpeg',
+      link: 'https://walnutandoak.com/desks/modern-neo-desk-oak',
+      company: 'Walnut x Oak',
+      attributes: {
+        price: '$645',
+        dimensions: '46 x 22 x 30"',
+      }
+    },
+    {
+      name: 'Etsy Pine Desk',
+      image: 'https://i.etsystatic.com/31395257/r/il/3551ed/3303931535/il_300x300.3303931535_dj20.jpg',
+      link: 'https://www.etsy.com/listing/1065473539/handmade-wooden-desk-office-desk-dining?ref=user_profile&frs=1',
+      company: 'Homezilla',
+      attributes: {
+        price: '$250',
+        dimensions: '47.25 x 23.6 x 30"',
+      }
+    },
+    {
+      name: 'Georg Desk',
+      image: 'https://cdn.shopify.com/s/files/1/1520/8686/products/1930349-Georg-Desk_d8f95bb3-4606-457d-9f8b-ef6a82a749e7_grande.jpg?v=1630079541',
+      link: 'https://www.designpublic.com/products/halstrom-georg-desk-skagerak-denmark?currency=USD&variant=38259772129465&utm_medium=cpc&utm_source=google&utm_campaign=Google%20Shopping&gclid=CjwKCAjwhOyJBhA4EiwAEcJdcVLEL1DTlRL_kZy6Dnw8liQykuV9air3_k_ZfMwU5cJv0u2XTZMQ0hoCn5IQAvD_BwE',
+      company: 'Skagerak',
+      attributes: {
+        price: '$1,100',
+        dimensions: '47 x 25 x 29"',
+      }
+    },
+    {
+      name: 'Oak custom desktop',
+      image: 'https://cdn11.bigcommerce.com/s-fylu6tzj32/images/stencil/1024x1024/products/118/23/White_Oak_Wood_Office_Desk_Top__04348.1590782588.jpg?c=2',
+      link: 'https://www.hardwooddesktops.com/products/white-oak-desktop-customize-order-online.html',
+      company: 'HardwoodDeskTops.com',
+      attributes: {
+        price: '$487',
+        dimensions: '49 x 22"',
+      }
+    },
+
+    {
+      name: 'Oak tapered legs',
+      image: 'https://i.etsystatic.com/14838206/r/il/a4864c/2406294867/il_794xN.2406294867_9hzt.jpg',
+      link: 'https://www.etsy.com/listing/818056251/oak-4-x-tapered-table-legs-oak-legs-set?gpla=1&gao=1&utm_source=google&utm_medium=cpc&utm_campaign=shopping_us_b-craft_supplies_and_tools-tools_and_equipment-home_improvement_and_hardware-moldings_and_trim&utm_custom1=_k_CjwKCAjw7fuJBhBdEiwA2lLMYUQ3E8RTlBHfiSM1wbVe7Zb2kIu492NHgWC1DaSG4jcaaGPZ5BOVahoCHz8QAvD_BwE_k_&utm_content=go_12569672873_124797503972_507438915509_pla-295462060307_c__818056251_117005547&utm_custom2=12569672873&gclid=CjwKCAjw7fuJBhBdEiwA2lLMYUQ3E8RTlBHfiSM1wbVe7Zb2kIu492NHgWC1DaSG4jcaaGPZ5BOVahoCHz8QAvD_BwE&variation0=2120702922',
+      company: 'Etsy',
+      attributes: {
+        price: '$100',
+        dimensions: '49 x 22"',
+      }
+    },
+
+  ],
+  'cactus planters': [
+    {
+      name: 'Countour Planter',
+      image: 'https://cdn.shopify.com/s/files/1/0020/8715/7807/products/REVISED2_9dd82771-b92d-4f41-a1a7-0d23161c67e0_720x.png?v=1629922897',
+      link: 'https://www.peachandpebble.com/products/forest-green-contour-planter?variant=31880847228995',
+      company: 'Peach & Pebble',
+      attributes: {
+        price: '$60 - $80',
+        diameter: '10 - 12"',
+        color: 'Forest green'
+      }
+    },
+    {
+      name: 'Bishop Pedstal Planter',
+      image: 'https://assets.weimgs.com/weimgs/rk/images/wcm/products/202112/0209/img3z.jpg',
+      link: 'https://www.westelm.com/products/bishop-pedestal-planters-d6246/?pkey=cindoor-planters&sku=cm_src=indoor-planters||NoFacet-_-NoFacet-_--_-&cm_src=reducedClick&isx=0.0.807',
+      company: 'West Elm',
+      attributes: {
+        price: '$129',
+        dimensions: '10.5 x 13.5"',
+        color: 'Marina Blue'
+      }
+    },
+    {
+      name: 'Parker Planter',
+      image: 'https://s7d5.scene7.com/is/image/UrbanOutfitters/40690406_010_e?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960',
+      link: 'https://www.urbanoutfitters.com/shop/parker-10-planter-drainage-tray-set?color=010&type=REGULAR&size=ONE%20SIZE&quantity=1',
+      company: 'Urban Outfitters',
+      attributes: {
+        price: '$60',
+        diameter: '10"',
+        color: 'White'
+      }
+    },
+    {
+      name: 'Juxton Mediun Planter',
+      image: 'https://cb.scene7.com/is/image/Crate/JuxtonPlanterMedBlueSSS21/$web_pdp_main_carousel_med$/210115090621/juxton-medium-blue-planter.jpg',
+      link: 'https://www.crateandbarrel.com/juxton-planters/f1001594',
+      company: 'Crate & Barrel',
+      attributes: {
+        price: '$20',
+        dimensions: '12 x 10"',
+        color: 'Blue'
+      }
+    },
+  ],
+  webcams: [
+    {
+      name: 'HD C920',
+      image: 'https://m.media-amazon.com/images/I/71iNwni9TsL._AC_SX679_.jpg',
+      link: 'https://www.amazon.com/Logitech-Widescreen-Calling-Recording-Desktop/dp/B006JH8T3S',
+      company: 'Logitech',
+      attributes: {
+        price: '$75',
+        resolution: '1080p',
+      }
+    },
+    {
+      name: 'HD C925-e',
+      image: 'https://m.media-amazon.com/images/I/51bfr5e1iRL._AC_SX679_.jpg',
+      link: 'https://www.amazon.com/Logitech-Webcam-Built-Stereo-Microphones/dp/B01DPNPJ72/ref=asc_df_B01DPNPJ72/?tag=hyprod-20&linkCode=df0&hvadid=241907595991&hvpos=&hvnetw=g&hvrand=8272151280138756391&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061095&hvtargid=pla-494133676788&psc=1',
+      company: 'Logitech',
+      attributes: {
+        price: '$71.25',
+        resolution: '1080p',
+      }
+    },
+  ],
+  throws: [
+    {
+      name: 'Cotton Waffle Throw, Golden Oak',
+      image: 'https://assets.weimgs.com/weimgs/rk/images/wcm/products/202124/0279/img5z.jpg',
+      link: 'https://www.westelm.com/products/cotton-waffle-throw-t6224/?catalogId=71&sku=4639245&cm_ven=PLA&cm_cat=Google&cm_pla=Pillows%20%26%20Decor%20%3E%20Throws&region_id=669090&cm_ite=4639245&gclid=CjwKCAjwhOyJBhA4EiwAEcJdcSnk-iSbf1KqxWLANtE4QZui02dW-uYd5-hjrybfzuuxo1c1U5WEaBoCFvoQAvD_BwE',
+      company: 'West Elm',
+      attributes: {
+        price: '$50',
+        dimensions: '50 x 60"',
+      }
+    },
+  ],
+  'knick knacks': [
+    {
+      name: 'Toothbrush holder',
+      image: 'https://cdn.shopify.com/s/files/1/0223/7523/5648/products/pretti.cool_Studio_March2021_Square_Web_13_1024x1024@2x.jpg?v=1626293091',
+      link: 'https://pretti.cool/collections/new-arrivals/products/toothbrush-holders',
+      company: 'pretti.cool',
+      attributes: {
+        price: '$32.00',
+        dimensions: '2.5" w x 3" h'
+      }
+    },
+    {
+      name: '4" vessel',
+      image: 'https://cdn.shopify.com/s/files/1/0223/7523/5648/products/pretti_cool_Product_Square_Web_220_1024x1024@2x.jpg?v=1593543080',
+      link: 'https://pretti.cool/collections/all-products/products/4-vessels?variant=25950116970560',
+      company: 'pretti.cool',
+      attributes: {
+        price: '$32',
+        dimensions: '4" w x 2 7/8" h'
+      }
+    },
+    {
+      name: 'Matchstick holder',
+      image: 'https://cdn.shopify.com/s/files/1/0223/7523/5648/products/pretti_cool_styled_June_24_Square_21_1024x1024@2x.jpg?v=1610223411',
+      link: 'https://pretti.cool/products/matchstick-holder?variant=30284790661184',
+      company: 'pretti.cool',
+      attributes: {
+        price: '$28',
+        dimensions: '2.5" w x 1.875" h'
+      }
+    },
+  ],
+  beds: [
+    {
+      name: 'Dris bed',
+      image: 'https://cdn.sanity.io/images/qajfl69b/production/92185ef169f333e6a40cfcca6e9ab1cad08c108f-2187x2187.jpg',
+      link: '',
+      company: 'Akron Street',
+      attributes: {
+        price: '$1000',
+      }
+    },
+    {
+      name: 'Oak Bed',
+      image: 'https://cb.scene7.com/is/image/Crate/TuftNeedleOakBedFrm3QSSF20_VND/$web_pdp_main_carousel_med$/210910081422/TuftNeedleOakBedFrm3QSSF20_VND.jpg',
+      link: 'https://www.tuftandneedle.com/frame/',
+      company: 'Tufts & Needs',
+      attributes: {
+        price: '$1100',
+      }
+    },
+  ],
+}
+
+
+class Fragment {
+
+ constructor() {
+   this.fragment = document.createDocumentFragment();
+ }
+
+ #addHeader(name) {
+   const header = document.createElement('h1');
+   header.innerHTML = name;
+   this.fragment.appendChild(header);
+ }
+
+ #createEl(el) {
+   return document.createElement(el);
+ }
+
+ #addItem(item) {
+   // Create figure
+   const figure = this.#createEl('figure');
+   figure.innerHTML = `
+    <img src=${item.image}>
+   `
+   let figcaption = this.#createEl('figcaption');
+   figure.appendChild(figcaption);
+   figcaption.innerHTML = `<a target="_blank" href=${item.link}><b>${item.company} | ${item.name}</b></a></br>`
+
+   const attributes = item.attributes;
+   for (let attribute of Object.keys(attributes)) {
+     figcaption.innerHTML += `${attributes[attribute]}</br>`
+   }
+   return figure;
+ }
+
+ addSection(sectionObj, name) {
+   this.#addHeader(name);
+   const sectionEl = document.createElement('section');
+   sectionObj.forEach(item => {
+     const figure = this.#addItem(item);
+     sectionEl.appendChild(figure);
+   });
+   this.fragment.appendChild(sectionEl);
+ }
+
+ populateDOM() {
+   document.body.appendChild(this.fragment);
+ }
+}
+
+var fragment = new Fragment();
+
+const sections = Object.keys(data);
+
+sections.forEach(section => {
+  sectionObj = data[section];
+  fragment.addSection(sectionObj, section);
+});
+
+fragment.populateDOM();
+
+document.getElementById('darkMode').addEventListener('click', () => {
+
+})
